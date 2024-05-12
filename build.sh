b231fc7fc5a9f60f7c3798d7c921a94e155eca60
Phil Hunt <phil.hunt@independentid.com>
2024-05-12 09:58:46 -0700

    Enhance build.sh with -c option to install Hexa CLI
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "This builds a docker image for local execution"
 tag="hexaopa"
 test="N"
 doPush="N"
-optString="bpt:"
+optString="hbpct:"
 while getopts ${optString} OPTION; do
   case "$OPTION" in
     b)
@@ -18,14 +18,26 @@ while getopts ${optString} OPTION; do
       echo "Push requested."
       doPush="Y"
       ;;
+    c)
+      echo "Installing Hexa CLI..."
+      if ! command -v hexa &> /dev/null
+      then
+          go install github.com/hexa-org/policy-mapper/cmd/hexa@latest
+          exit 1
+      fi
+      hexa help
+      exit
+      ;;
     *)
       echo "Usage: ./build.sh -b -t <tag> -p"
       echo "  -b         Performs build and test (default: build only)"
       echo "  -t <value> Builds the docker image with the specified tag (default: hexaopa)"
       echo "  -p         Push the image to docker (default: not pushed)"
+      echo "  -c         Check and install the Hexa CLI from github.com/hexa-org/policy-mapper"
       exit 1
   esac
 done
+
 
 if [ "$test" = 'Y' ];then
     echo "  building and running tests ..."
